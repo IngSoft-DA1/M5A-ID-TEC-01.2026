8f784f3042354c2c1c6f746ebaa925b5ad2e3217
marcos <marcosbuydid@gmail.com>
2026-03-24 23:38:22 -0300

    add exercise

---
+++ exercises/17-03-26/DA12025/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using Domain;
+
+namespace ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Type your name: ");
+        string? inputName = Console.ReadLine();
+
+        Console.Write("Type your birthday (yyyy-MM-dd): ");
+        string? inputBirthday = Console.ReadLine();
+
+        if (!DateTime.TryParseExact(inputBirthday, "yyyy-MM-dd", CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out DateTime birthday) || String.IsNullOrEmpty(inputName))
+        {
+            Console.WriteLine("Input name, birthday or both are invalid. Run the program again.");
+        }
+        else
+        {
+            Person person = new Person(inputName, birthday);
+
+            Console.WriteLine($"\nHello, {person.Name}!");
+            Console.WriteLine($"You born on {person.GetWeekDayOfBorn()}.");
+            Console.WriteLine($"Your are {person.CalculateAge()} years old.");
+        }
+    }
+}
---
+++ exercises/17-03-26/DA12025/Domain/Person.cs
@@ -0,0 +1,42 @@
+﻿namespace Domain;
+
+public class Person
+{
+    private string _name;
+    private DateTime _birthday;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value; 
+    }
+
+    public DateTime Birthday
+    {
+        get => _birthday;
+        set => _birthday = value;
+    }
+
+    public Person(string name, DateTime birthday)
+    {
+        Name = name;
+        Birthday = birthday;
+    }
+
+    public int CalculateAge()
+    {
+        DateTime today = DateTime.Today;
+        int age = today.Year - Birthday.Year;
+        if (Birthday > today.AddYears(-age))
+        {
+            age--;
+        }
+
+        return age;
+    }
+
+    public string GetWeekDayOfBorn()
+    {
+        return Birthday.DayOfWeek.ToString();
+    }
+}
