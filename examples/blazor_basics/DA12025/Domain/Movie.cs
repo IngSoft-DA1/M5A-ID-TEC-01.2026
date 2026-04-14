c53d500607cf77964d0dfda1a87747562cda3e86
marcos <marcosbuydid@gmail.com>
2026-04-13 22:36:31 -0300

    add blazor basic example and unit test markdown file

---
+++ examples/blazor_basics/DA12025/Domain/Movie.cs
@@ -0,0 +1,33 @@
+﻿namespace Domain;
+
+public class Movie
+{
+    private string _title;
+    private string _director;
+    private DateTime _releaseDate;
+
+    public string Title
+    {
+        get => _title;
+        set => _title = value;
+    }
+
+    public string Director
+    {
+        get => _director;
+        set => _director = value;
+    }
+
+    public DateTime ReleaseDate
+    {
+        get => _releaseDate;
+        set => _releaseDate = value;
+    }
+
+    public Movie(string title, string director, DateTime releaseYear)
+    {
+        Title = title;
+        Director = director;
+        ReleaseDate = releaseYear;
+    }
+}
