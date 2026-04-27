8da2191cae7f024d93d1b96325e05edc882bab1d
marcos <marcosbuydid@gmail.com>
2026-04-27 20:29:35 -0300

    add blazor full example

---
+++ examples/blazor_full_example/DA12026/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+
+namespace Services.Interfaces;
+
+public interface IMovieService
+{
+    List<Movie> GetMovies();
+    void AddMovie(Movie movie);
+    Movie GetMovie(string title);
+}
