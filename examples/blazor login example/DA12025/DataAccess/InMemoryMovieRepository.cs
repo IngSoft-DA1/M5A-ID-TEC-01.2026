8c8dc929a1170e09eca0ef8cc96565059273f3d6
marcos <marcosbuydid@gmail.com>
2026-05-04 19:33:40 -0300

    add login example

---
+++ examples/blazor login example/DA12025/DataAccess/InMemoryMovieRepository.cs
@@ -0,0 +1,50 @@
+﻿using Domain;
+
+namespace DataAccess
+{
+    public class InMemoryMovieRepository
+    {
+        private List<Movie> Movies { get; }
+
+        public InMemoryMovieRepository()
+        {
+            Movies = new List<Movie>();
+            LoadDefaultMovies();
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return Movies;
+        }
+
+        public Movie? GetMovie(string title)
+        {
+            Movie? movie = Movies.FirstOrDefault(movie => movie.Title == title);
+            return movie;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            Movies.Add(movie);
+        }
+
+        public void DeleteMovie(Movie movie)
+        {
+            Movies.Remove(movie);
+        }
+
+        public void UpdateMovie(Movie movieToUpdate)
+        {
+            Movie? movie = Movies.Find(m => m.Title == movieToUpdate.Title);
+            var movieToUpdateIndex = Movies.IndexOf(movie);
+            Movies[movieToUpdateIndex] = movieToUpdate;
+        }
+
+        private void LoadDefaultMovies()
+        {
+            Movies.Add(new Movie("Black Rain", "Ridley Scott", new DateTime(1989, 09, 22), 30000000));
+            Movies.Add(new Movie("Cast Away", "Robert Zemeckis", new DateTime(2000, 12, 22), 25000000));
+            Movies.Add(new Movie("Training Day", "Antoine Fuqua", new DateTime(2002, 01, 18), 10000000));
+        }
+    }
+}
---
+++ examples/blazor login example/DA12025/Services/MovieService.cs
@@ -0,0 +1,101 @@
+﻿using DataAccess;
+using Domain;
+using Services.Interfaces;
+using Services.Models;
+
+namespace Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly InMemoryMovieRepository _movieRepository;
+
+        public MovieService(InMemoryMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public void AddMovie(MovieDTO movie)
+        {
+            ValidateUniqueTitle(movie.Title);
+            _movieRepository.AddMovie(ToEntity(movie));
+        }
+
+        public void DeleteMovie(string title)
+        {
+            Movie? movieToDelete = _movieRepository.GetMovie(title);
+            if (movieToDelete == null)
+            {
+                throw new ArgumentException("Cannot find the specified movie");
+            }
+
+            _movieRepository.DeleteMovie(movieToDelete);
+        }
+
+        public List<MovieDTO> GetMovies()
+        {
+            List<MovieDTO> moviesDTO = new List<MovieDTO>();
+
+            foreach (var movie in _movieRepository.GetMovies())
+            {
+                moviesDTO.Add(FromEntity(movie));
+            }
+
+            return moviesDTO;
+        }
+
+        public void UpdateMovie(MovieDTO movieToUpdate)
+        {
+            Movie? movie = _movieRepository.GetMovie(movieToUpdate.Title);
+            if (movie == null)
+            {
+                throw new ArgumentException("Cannot find the specified movie");
+            }
+
+            movie.Title = movieToUpdate.Title;
+            movie.Director = movieToUpdate.Director;
+            movie.ReleaseDate = movieToUpdate.ReleaseDate;
+            //to avoid budget value be on cero after update
+            movieToUpdate.Budget = movie.Budget;
+            _movieRepository.UpdateMovie(movie);
+        }
+
+        public MovieDTO GetMovie(string title)
+        {
+            Movie? movie = _movieRepository.GetMovie(title);
+            if (movie == null)
+            {
+                throw new ArgumentException("Cannot find movie with this title");
+            }
+
+            return FromEntity(movie);
+        }
+
+        private void ValidateUniqueTitle(string title)
+        {
+            string inputTitle = title.Trim().ToLowerInvariant();
+            foreach (var movie in _movieRepository.GetMovies())
+            {
+                string retrievedTitle = movie.Title.Trim().ToLowerInvariant();
+                if (retrievedTitle == inputTitle)
+                {
+                    throw new ArgumentException("There`s a movie already defined with that title");
+                }
+            }
+        }
+
+        private static Movie ToEntity(MovieDTO movieDTO)
+        {
+            return new Movie(movieDTO.Title, movieDTO.Director, movieDTO.ReleaseDate, movieDTO.Budget);
+        }
+
+        private static MovieDTO FromEntity(Movie movie)
+        {
+            return new MovieDTO()
+            {
+                Title = movie.Title,
+                Director = movie.Director,
+                ReleaseDate = movie.ReleaseDate
+            };
+        }
+    }
+}
