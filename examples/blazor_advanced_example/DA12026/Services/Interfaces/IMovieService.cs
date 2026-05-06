97627fd2e3433c93bd91ed1797d7f57e1667340d
marcos <marcosbuydid@gmail.com>
2026-05-05 23:22:42 -0300

    add blazor advanced project example

---
+++ examples/blazor_advanced_example/DA12026/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,12 @@
+﻿using Services.Models;
+
+namespace Services.Interfaces;
+
+public interface IMovieService
+{
+    List<MovieDTO> GetMovies();
+    void AddMovie(MovieDTO movie);
+    MovieDTO GetMovie(string title);
+    void DeleteMovie(string title);
+    void UpdateMovie(MovieDTO movie);
+}
